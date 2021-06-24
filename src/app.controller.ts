22159fada16a9e5015fdcb6857851b88bd01fbb6
KimGenius <geniusk1047@naver.com>
2021-06-24 13:02:19 +0900

    insert 성능 비교 부분

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -26,7 +26,6 @@ export class AppController {
     return result
   }
 
-
   @Get('/users/:id')
   async getUserById(@Param('id') id: string): Promise<UserModel> {
     return this.prismaService.user.findUnique({ where: { id: Number(id) } })
@@ -35,13 +34,46 @@ export class AppController {
   @Post('/users')
   async saveUser(@Param('id') id: string) {
     const data = []
-    for (let i = 0; i < 31000; i++) {
+    for (let i = 0; i < 30000; i++) {
       data.push({
         name: '김영재',
-        email: `${Math.random().toString(36).substr(2,11)}@${Math.random().toString(36).substr(2,11)}.com`,
+        email: `${Math.random().toString(36).substr(2, 11)}@${Math.random()
+          .toString(36)
+          .substr(2, 11)}.com`,
       })
     }
+    console.time('Query')
     await this.prismaService.user.createMany({ data })
+    console.timeEnd('Query')
+  }
+
+  @Post('/users-raw')
+  async saveUserRaw() {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const mysql = require('mysql2')
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'Rladudwo132!3@',
+      database: 'node_orm',
+    })
+    connection.connect()
+    let data = ''
+    for (let i = 0; i < 30000; i++) {
+      data += `("김영재", "${Math.random()
+        .toString(36)
+        .substr(2, 11)}@${Math.random().toString(36).substr(2, 11)}.com"), `
+    }
+    data = data.substr(0, data.length - 2)
+    console.time('Query')
+    connection.query(
+      `INSERT INTO user(name, email) VALUES ${data}`,
+      function (error, results, fields) {
+        if (error) throw error
+        console.timeEnd('Query')
+        connection.end()
+      },
+    )
   }
 
   @Put('/users/:id')
@@ -67,5 +99,23 @@ export class AppController {
       where: { id: Number(id) },
       include: { posts: true },
     })
+  }
+
+  @Post('/users/:id/posts')
+  async saveUserPost() {
+    const createUserQuery = this.prismaService.user.create({
+      data: {
+        name: '김영재',
+        email: `${Math.random().toString(36).substr(2, 11)}@${Math.random()
+          .toString(36)
+          .substr(2, 11)}.com`,
+      },
+    })
+    const createPostQuery = this.prismaService.post.create({
+      data: {
+        title: '김영재',
+      },
+    })
+    await this.prismaService.$transaction([createUserQuery, createPostQuery])
   }
 }
